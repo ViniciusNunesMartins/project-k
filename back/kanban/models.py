41363558b5e2957f9c0bf4f98342deb2642d79ce
Vinicius Nunes Martins <nunesmartinsvinicius@gmail.com>
2021-10-11 19:28:38 +0000

    files kanban app

--- back/kanban/models.py
+++ back/kanban/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel, TitleDescriptionModel
+from django.conf import settings 
 
-# Create your models here.
+
+class Dashboard(models.Model, TimeStampedModel):
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+
+class Tasks(models.Model, TitleDescriptionModel, TimeStampedModel):
+    dashboard = models.ForeignKey(Dashboard, on_delete=models.CASCADE)
+    status = models.CharField(max_length=5, choices=(
+        ("TO DO", "TO_DO"),
+        ("DOING", "DOING"),
+        ("DONE", "DONE")
+    ))
+    
