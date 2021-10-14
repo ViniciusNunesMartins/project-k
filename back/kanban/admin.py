d3742ca942be480a033e6bf9a381276594e83790
Vinicius Nunes Martins <nunesmartinsvinicius@gmail.com>
2021-10-14 01:51:15 +0000

    update kanban models

--- back/kanban/admin.py
+++ back/kanban/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Dashboard, Tasks
 
-# Register your models here.
+
+@admin.register(Dashboard, Tasks)
+class KanbanAdmin(admin.ModelAdmin):
+    pass
--- back/kanban/models.py
+++ back/kanban/models.py
@@ -1,17 +1,27 @@
 from django.db import models
 from django_extensions.db.models import TimeStampedModel, TitleDescriptionModel
-from django.conf import settings 
+from django.conf import settings
+from django.utils import timezone
 
 
-class Dashboard(models.Model, TimeStampedModel):
+class Dashboard(TimeStampedModel, models.Model):
     name = models.CharField(max_length=50)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
 
-class Tasks(models.Model, TitleDescriptionModel, TimeStampedModel):
+    def __str__(self):
+        return self.name
+
+class Tasks(TitleDescriptionModel, TimeStampedModel, models.Model):
     dashboard = models.ForeignKey(Dashboard, on_delete=models.CASCADE)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="creator")
+    responsible = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="responsible")
+    deadline = models.DateField(default=timezone.now)
     status = models.CharField(max_length=5, choices=(
         ("TO DO", "TO_DO"),
         ("DOING", "DOING"),
         ("DONE", "DONE")
     ))
+
+    def __str__(self):
+        return f"{self.title} - {self.status}: {self.deadline}" 
     
